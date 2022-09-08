2758eb3cd7c0b529cab929bce10b418e62bb958e
Pavel Piasetski <pavel.piasetski@itechart-group.com>
2022-09-08 14:32:29 +0300

    Add login button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <div className={s.logoWrapper}>
                 <img className={s.logo} src="https://upload.wikimedia.org/wikipedia/commons/7/77/Arm_logo_2017.svg"/>
+            </div>
+            <div className={s.loginBlock}>
+                {
+                    props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>
+                }
             </div>
         </header>
     );
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import sidebarReducer from "./sidebarReducer";
 import usersReducer from "./usersReducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebarPage: sidebarReducer,
     usersPage: usersReducer,
+    auth: authReducer,
 });
 
 let store = createStore(reducers);
