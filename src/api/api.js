ecce755dd422bc7e15a149d024b16fcfc332eaa4
Pavel Piasetski <pavel.piasetski@itechart-group.com>
2022-09-12 21:28:09 +0300

    Create usersAPI

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import * as axios from "axios";
+import {API_SAMURAI_KEY} from "../redux/redux-store";
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseUrl: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        // 'Content-Type': 'application/json',
+        'API-KEY': API_SAMURAI_KEY,
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    }
+}
