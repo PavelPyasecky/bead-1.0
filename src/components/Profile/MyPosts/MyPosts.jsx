65504fae6d87a1e4d150022b2291a22feb59d4ae
Pavel Piasetski <pavel.piasetski@itechart-group.com>
2022-09-22 17:37:08 +0300

    React.memo add

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,36 +5,30 @@ import {Field, reduxForm} from "redux-form";
 import {maxLengthCreator, required} from "../../../utils/validators/validators";
 import {Textarea} from "../../common/FormsControls/FormsControls";
 
-const MyPosts = (props) => {
-    let postsElements = props.postsData.map(post => (
-        <Post message={post.message} likeCount={post.likesCount}/>
-    ))
+const MyPosts = React.memo(props => {
+    let postsElements = props.postsData.map(post => (<Post message={post.message} likeCount={post.likesCount}/>))
 
     let addNewPost = (formData) => {
         let newPostText = formData.newPostText;
         props.addPost(newPostText);
     }
 
-    return (
-        <div className={s.posts}>
+    return (<div className={s.posts}>
             <div className={s.postsForm}>
                 <AddNewPostFormRedux onSubmit={addNewPost}/>
             </div>
             {postsElements}
-        </div>
-    );
-}
+        </div>);
+});
 
 const maxLength10 = maxLengthCreator(10);
 
 const AddNewPostForm = (props) => {
-    return (
-        <form onSubmit={props.handleSubmit}>
+    return (<form onSubmit={props.handleSubmit}>
             <Field component={Textarea} name={'newPostText'} className={s.postsForm__text} value={props.newPostText}
                    validate={[required, maxLength10]}/>
             <button className={s.postsForm__btn}>Send</button>
-        </form>
-    );
+        </form>);
 }
 
 const AddNewPostFormRedux = reduxForm({form: 'ProfileAddNewPostForm'})(AddNewPostForm)
