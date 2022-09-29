1ea43cb1514103b2aa29d9cc81f3d28daf4719ba
Pavel Piasetski <pavel.piasetski@itechart-group.com>
2022-09-30 01:16:57 +0300

    Selectors

--- src/redux/users-Selectors.js
+++ src/redux/users-Selectors.js
@@ -1,11 +1,11 @@
 import {createSelector} from "reselect";
 
-const getUsersSelector = (state) => {
+export const getUsersSelector = (state) => {
     return state.usersPage.users;
 }
 
 export const getUsers = createSelector(getUsersSelector, (users) => {
-    users.filter(u => true);
+    return users.filter(u => true);
 })
 
 export const getPageSize = (state) => {
